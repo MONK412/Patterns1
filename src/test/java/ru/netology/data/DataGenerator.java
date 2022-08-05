605459da853d3ccf6249f7243e2bebbc50b3702c
Rustam Mustafin <rustammuus@mail.ru>
2022-08-05 19:51:43 +0300

    DataGenerator Faker Fix

--- src/test/java/ru/netology/data/DataGenerator.java
+++ src/test/java/ru/netology/data/DataGenerator.java
@@ -8,27 +8,29 @@ import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
 public class DataGenerator {
+
+    private static final Faker faker = new Faker(new Locale("ru"));
     private DataGenerator() {
     }
+
 
     public static String generateDate(int shift) {
         return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     public static String generateCity(String locale) {
-        Faker faker = new Faker(new Locale("ru"));
-        return faker.address().city();
+        String city = faker.address().city();
+        return city;
     }
 
     public static String generateName(String locale) {
-        Faker faker = new Faker(new Locale("ru"));
-        return faker.name().lastName() + " " + faker.name().firstName();
+        String name = faker.name().lastName() + " " + faker.name().firstName();
+        return name;
     }
 
     public static String generatePhone(String locale) {
-        Faker faker = new Faker(new Locale("ru"));
-        faker.numerify("+7##########");
-        return faker.phoneNumber().phoneNumber();
+        String phone = faker.numerify("+7##########");
+        return phone;
     }
 
     public static class Registration {
